30a1f5d360b1660cfa4c6668b9657d2736faf98c
Fernando Gomes <lfgsnando@gmail.com>
2020-04-14 20:05:08 -0300

    Initial Commit

---
+++ MovieCRUD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieCRUD
+//
+//  Created by Fernando Gomes on 14/04/20.
+//  Copyright © 2020 Fernando Gomes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
